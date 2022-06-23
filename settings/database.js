e0682d2176b29b9561739b33f68311201efb943d
Kirill Lisovskiy <lisovskiy.322@gmail.com>
2022-06-24 00:09:53 +0300

    pool

--- settings/database.js
+++ settings/database.js
@@ -1,16 +1,24 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+/* const connection = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b04f3e091ff39f',
     password: '6468eb76',
     database: 'heroku_eda79bf60c6eb17'
-});
+}); */
 
-connection.connect((error) => {
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b04f3e091ff39f',
+    password: '6468eb76',
+    database: 'heroku_eda79bf60c6eb17',
+})
+
+pool.connect((error) => {
     if (error) {
         return console.log('-----DataBase not connected-----');
     } return console.log('-----DataBase Connected-----');
 })
 
-module.exports = connection;
+module.exports = pool;
