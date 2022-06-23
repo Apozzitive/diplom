a7ae4986e267afae3364ce4200f4bfaa17e20fca
Kirill Lisovskiy <lisovskiy.322@gmail.com>
2022-06-23 23:29:22 +0300

    ad

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 8016;
+const port = 8016;
 const bodyParser = require('body-parser');
 
 
