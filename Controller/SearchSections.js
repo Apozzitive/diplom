e44c45810e8b28391f7e566aa0866c0b608345b4
Kirill Lisovskiy <lisovskiy.322@gmail.com>
2022-06-24 20:34:27 +0300

    312

--- Controller/SearchSections.js
+++ Controller/SearchSections.js
@@ -6,17 +6,17 @@ const server = require('../server');
 
 var db = require('../settings/database');
 exports.sections = (req, res) => {
-    db.query(`SELECT topic_title, topic_content FROM section_1"
+    db.query(`SELECT topic_title, topic_content FROM section_1
     UNION
-    SELECT topic_title, topic_content FROM section_2"
+    SELECT topic_title, topic_content FROM section_2
     UNION
-    SELECT topic_title, topic_content FROM section_3"
+    SELECT topic_title, topic_content FROM section_3
     UNION
-    SELECT topic_title, topic_content FROM section_4"
+    SELECT topic_title, topic_content FROM section_4
     UNION
-    SELECT topic_title, topic_content FROM section_5"
+    SELECT topic_title, topic_content FROM section_5
     UNION
-    SELECT topic_title, topic_content FROM section_6"`, (error, rows) => {
+    SELECT topic_title, topic_content FROM section_6`, (error, rows) => {
         if (error) {
             console.log(error);
         } else {
