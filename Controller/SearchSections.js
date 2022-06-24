9344162a7b7badf9f0119be5d1b70ba67a927971
Kirill Lisovskiy <lisovskiy.322@gmail.com>
2022-06-24 20:15:46 +0300

    321

--- Controller/SearchSections.js
+++ Controller/SearchSections.js
@@ -6,17 +6,17 @@ const server = require('../server');
 
 var db = require('../settings/database');
 exports.sections = (req, res) => {
-    db.query(`SELECT topic_title, topic_content FROM section_1 WHERE topic_content LIKE "%${server.inputValue}%"
+    db.query(`SELECT topic_title, topic_content FROM section_1"
     UNION
-    SELECT topic_title, topic_content FROM section_2 WHERE topic_content LIKE "%${server.inputValue}%"
+    SELECT topic_title, topic_content FROM section_2"
     UNION
-    SELECT topic_title, topic_content FROM section_3 WHERE topic_content LIKE "%${server.inputValue}%"
+    SELECT topic_title, topic_content FROM section_3"
     UNION
-    SELECT topic_title, topic_content FROM section_4 WHERE topic_content LIKE "%${server.inputValue}%"
+    SELECT topic_title, topic_content FROM section_4"
     UNION
-    SELECT topic_title, topic_content FROM section_5 WHERE topic_content LIKE "%${server.inputValue}%"
+    SELECT topic_title, topic_content FROM section_5"
     UNION
-    SELECT topic_title, topic_content FROM section_6 WHERE topic_content LIKE "%${server.inputValue}%"`, (error, rows) => {
+    SELECT topic_title, topic_content FROM section_6"`, (error, rows) => {
         if (error) {
             console.log(error);
         } else {
