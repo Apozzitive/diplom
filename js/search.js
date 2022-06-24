f44f96600eb2e698035c16b57580eacf2521a2cd
Kirill Lisovskiy <lisovskiy.322@gmail.com>
2022-06-24 21:14:16 +0300

    321

--- js/search.js
+++ js/search.js
@@ -16,7 +16,7 @@ async function getResponseSearch() {
     let valInp = '';
     valInp = document.querySelector('.search__input').value;
     let valueInput = valInp.toLowerCase();
-    if (searchContent.values[0] === undefined || searchContent.values[0].topic_content !== valueInput) {
+    if (searchContent.values[0] === undefined || searchContent.values[0].topic_content !== valInp) {
         const notFound = "Ничего не найдено";
         const divSearchResult = document.createElement('div');
         divSearchResult.className = "search__result-result";
